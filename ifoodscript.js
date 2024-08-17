2bd960f57fa9cfbceb77063c2c3d107726ff428a
ValfranJr <108398446+ValfranJr@users.noreply.github.com>
2024-08-16 23:58:27 -0300

    Create ifoodscript.js

---
+++ ifoodscript.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ifoodCalculatorForm = document.getElementById('ifood-calculator-form');
+
+    ifoodCalculatorForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const valorProduto = parseFloat(document.getElementById('valor-produto').value);
+        const tipoEntrega = parseFloat(document.getElementById('tipo-entrega').value);
+
+        const valorFinal = valorProduto / (1 - tipoEntrega);
+        document.getElementById('resultado-ifood').textContent = `O valor do produto no iFood é R$${valorFinal.toFixed(2)}`;
+    });
+});
